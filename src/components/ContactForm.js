226906b3125506d1ca2ccb7d503efb1bababdace
Gururaj <gururaj.upparagummala@geappliances.com>
2021-09-05 16:34:42 +0530

    input styling change

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,40 +4,40 @@ function ContactForm() {
             <div className="grid grid-cols-1 gap-6">
                 <label className="block">
                     <span className="text-gray-700">First name</span>
-                    <input type="text" name="firstName" className="block w-full mt-1" />
+                    <input type="text" name="firstName" className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" />
                 </label>
-                <label class="block">
+                <label className="block">
                     <span className="text-gray-700">Last name</span>
-                    <input type="text" name="lastName" className="block w-full mt-1" />
+                    <input type="text" name="lastName" className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" />
                 </label>
-                <label class="block">
+                <label className="block">
                     <span className="text-gray-700">Company</span>
-                    <input type="text" name="company" className="block w-full mt-1" />
+                    <input type="text" name="company" className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" />
                 </label>
 
-                <label class="block">
+                <label className="block">
                     <span className="text-gray-700">Mobile</span>
-                    <input type="number" name="mobile" className="block w-full mt-1" />
+                    <input type="number" name="mobile" className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" />
                 </label>
 
-                <label class="block">
+                <label className="block">
                     <span className="text-gray-700">Email</span>
-                    <input type="email" name="mobile" className="block w-full mt-1" />
+                    <input type="email" name="mobile" className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" />
                 </label>
 
-                <label class="block">
+                <label className="block">
                     <span className="text-gray-700">URL</span>
-                    <input type="url" name="url" className="block w-full mt-1" />
+                    <input type="url" name="url" className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" />
                 </label>
 
-                <label class="block">
+                <label className="block">
                     <span className="text-gray-700">Address</span>
-                    <textarea  name="address" className="block w-full mt-1"></textarea>
+                    <textarea name="address" className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black"></textarea>
                 </label>
 
-                <label class="block">
+                <label className="block">
                     <span className="text-gray-700">Birthday</span>
-                    <input type="date" name="dob" className="block w-full mt-1" />
+                    <input type="date" name="dob" className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" />
                 </label>
             </div>
 
